5d71629ee230a28f1e5f8beee9f24e81d62e2907
ThBuKj <thomas.bubere@hotmail.com>
2026-03-03 21:14:26 +0100

    feat(terraform): ta till en ny VM för att kunna ha till skolan

--- terraform/proxmox/main.tf
+++ terraform/proxmox/main.tf
@@ -45,3 +45,53 @@ resource "proxmox_virtual_environment_vm" "k3s_control_plane" {
     }
   }
 }
+
+
+
+resource "proxmox_virtual_environment_vm" "test" {
+  for_each = var.test_vms
+
+  name      = each.key
+  node_name = var.target_node
+
+  agent {
+    enabled = false
+  }
+
+  clone {
+    vm_id = var.template_id
+  }
+
+  cpu {
+    cores = each.value.cores
+  }
+
+  memory {
+    dedicated = each.value.memory
+  }
+
+  disk {
+    datastore_id = "local-lvm"
+    interface    = "scsi0"
+    size         = each.value.disk
+  }
+
+  network_device {
+    bridge = "vmbr0"
+  }
+
+  initialization {
+    ip_config {
+      ipv4 {
+        address = "${each.value.ip}/24"
+        gateway = "192.168.10.1"
+      }
+    }
+
+    user_account {
+      username = "ubuntu"
+      password = "ubuntu"
+      keys     = [file("~/.ssh/id_rsa_4096.pub")]
+    }
+  }
+}
