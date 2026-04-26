5ad92bdcff1ad983385c673a0fc05f9b04a1405c
ThBuKj <thomas.bubere@hotmail.com>
2026-04-26 21:49:07 +0200

    feat(cloudflared): migrate tunnel from k8s to router

---
+++ terraform/proxmox/lxc.tf
@@ -0,0 +1,62 @@
+resource "proxmox_virtual_environment_container" "arr_stack" {
+  node_name = "pve3"
+  vm_id     = 200
+
+  unprivileged = true
+
+  features {
+    nesting = true
+  }
+
+  initialization {
+    hostname = "arr-stack"
+
+    dns {
+      servers = ["1.1.1.1", "8.8.8.8"]
+    }
+
+    ip_config {
+      ipv4 {
+        address = "192.168.10.40/24"
+        gateway = "192.168.10.1"
+      }
+    }
+
+    user_account {
+      keys = [file(pathexpand(var.ssh_public_key_path))]
+    }
+  }
+
+  network_interface {
+    name   = "eth0"
+    bridge = "vmbr0"
+  }
+
+  operating_system {
+    template_file_id = "local:vztmpl/ubuntu-24.04-standard_24.04-2_amd64.tar.zst"
+    type             = "ubuntu"
+  }
+
+  disk {
+    datastore_id = "local-lvm"
+    size         = 32
+  }
+
+  mount_point {
+    path   = "/mnt/media"
+    volume = "local-lvm"
+    size   = "500G"
+  }
+
+  cpu {
+    cores = 4
+  }
+
+  memory {
+    dedicated = 6144
+  }
+
+  lifecycle {
+    ignore_changes = [initialization]
+  }
+}
