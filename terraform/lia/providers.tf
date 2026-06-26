0cae1bc4ef845f1f09970db71d7028e845977ef6
ThBuKj <thomas.bubere@hotmail.com>
2026-06-26 21:18:18 +0200

    feat(proxmox): Almalinux

---
+++ terraform/lia/providers.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = ">= 0.50.0"
+    }
+  }
+}
+
+provider "proxmox" {
+  endpoint  = var.proxmox_api_url
+  api_token = "${var.proxmox_api_token_id}=${var.proxmox_api_token_secret}"
+  # TODO: Set insecure = false and configure cacert for proper TLS verification.
+  insecure = true
+}
